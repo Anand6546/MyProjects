1997cd67ce3ad01718d8e56bc53a7af22f9db27d
ANAND NAGIDI <anand.nagidi@dxc.com>
2020-08-02 23:22:52 +0530

    added a dot net project

---
+++ MyProjects/MyMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GOD()
+        {
+            return View();
+        }
+        public ActionResult RGV()
+        {
+            return View();
+        }
+        public ActionResult Covid()
+        {
+            return View();
+        }
+        public ActionResult BruceLee()
+        {
+            return View();
+        }       
+    }
+}
---
+++ MyProjects/MyMVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApp.Models
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmplyeeMailID { get; set; }
+        public int EmployeeAge { get; set; }
+    }
+}
